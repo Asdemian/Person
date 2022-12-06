6d16cdc4c15f34df28b88777d6f823fc7fbc6d8b
ASD <demaniks@yahoo.com>
2022-12-06 21:18:46 +0300

    ДЗ1 human

---
+++ src/human.java
@@ -0,0 +1,10 @@
+public class human {
+    int yearOfBirth;
+    String name;
+    String town;
+
+    void greeting() {
+        System.out.println("Привет! Меня зовут " + name + " Я из города " + town + " Я родился в "
+                + yearOfBirth + " году. Будем знакомы!");
+    }
+}
